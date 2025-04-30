59b2e1f1a2ae170193fbeab8f8c7f4835560c923
fkara <fkara@EKSENIT>
2025-04-30 14:19:08 +0300

    Proje dosyası ekle.

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _movieContext;
+        public UpdateMovieCommandHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+        public async void Handler(UpdateMovieCommand command)
+        {
+            var value = await _movieContext.Movies.FindAsync(command.MovieId);
+
+            value.Rating = command.Rating;
+            value.Status = command.Status;
+            value.Duration = command.Duration;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.CreatedYear = command.CreatedYear;
+            value.Description = command.Description;
+            value.RelaseDate = command.RelaseDate;
+            value.Title = command.Title;
+             
+            await _movieContext.SaveChangesAsync();
+
+        }
+    }
+}
